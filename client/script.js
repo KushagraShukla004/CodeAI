f59dc94902d1ba65ff5ec08270d1f84aaac82493
Kushagra_Shukla <73269482+KushagraShukla004@users.noreply.github.com>
2022-12-31 01:44:18 +0530

    Server hosted

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from the server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codeai-fmyv.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
--- server/server.js
+++ server/server.js
@@ -45,5 +45,5 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () =>
-  console.log('Server is running on port http://localhost:5000')
+  console.log('Server is running on port https://codeai-fmyv.onrender.com')
 );
